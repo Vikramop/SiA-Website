d07c42c6bc5ea4ec09b38555bafe0ba76eb75549
Veervikram singh <veervikramsingh132@gmail.com>
2023-02-10 15:20:37 +0530

    navbar done

--- my-app/sections/Hero.jsx
+++ my-app/sections/Hero.jsx
@@ -1,8 +1,38 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.ypaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      varients={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Silicon
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>Asso</h1>
+          <div className={styles.heroDtext} />
+          <h1 className={styles.heroHeading}>ciation</h1>
+        </motion.div>
+      </div>
+
+      <motion.div
+        varients={slideIn('right', 'tween', 0.2, 1)}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      />
+    </motion.div>
   </section>
 );
 
