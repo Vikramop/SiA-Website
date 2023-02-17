6292a32e18aeda80cd0eebcb654a3ecdebd756e2
Veervikram singh <veervikramsingh132@gmail.com>
2023-02-17 22:21:56 +0530

    explore

--- my-app/sections/Explore.jsx
+++ my-app/sections/Explore.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+import { ExploreCard, TypingText, TypingText } from '../components';
+
 const Explore = () => <section>Evolve section</section>;
 
 export default Explore;
