7923a0e9927c10cc5e05f2c5cc959c268a375929
Veervikram singh <veervikramsingh132@gmail.com>
2023-02-18 22:12:52 +0530

    explore

--- my-app/components/ExploreCard.jsx
+++ my-app/components/ExploreCard.jsx
@@ -1,9 +1,19 @@
 'use client';
 
-const ExploreCard = () => (
-  <div>
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
+
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
+  <motion.div
+    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+    className={`relative ${
+      active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+  >
     Explore Card
-  </div>
+  </motion.div>
 );
 
 export default ExploreCard;
--- my-app/sections/Explore.jsx
+++ my-app/sections/Explore.jsx
@@ -4,9 +4,46 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { fadeIn, staggerContainer } from '../utils/motion';
-import { ExploreCard, TypingText, TypingText } from '../components';
+import { exploreWorlds } from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { ExploreCard, TitleText, TypingText } from '../components';
 
-const Explore = () => <section>Evolve section</section>;
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
+
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| Events" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              We run our events thorough out <br className="md:block hidden" />{' '}
+              the year
+            </>
+          }
+          textStyles="text-center"
+        />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
